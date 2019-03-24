a375190c58191042a989f67a0470fab39b2b94e3
Nikolaos Antonakakis <nikolaos.antonakakis@wu.ac.at>
2019-03-24 08:16:09 +0100

    Complete RecipeSelectionView

--- GoodEatins/Controller/RecipesSelectionVC.swift
+++ GoodEatins/Controller/RecipesSelectionVC.swift
@@ -8,16 +8,42 @@
 
 import UIKit
 
-class RecipesSelectionVC: UIViewController {
+class RecipesSelectionVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
     var selectedCategory: String!
+    var recipes: [Recipe]!
+    let data = DataSet()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        recipes = data.getRecipes(forCategoryTitle: selectedCategory)
 
         print(selectedCategory)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return recipes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "recipeCell", for: indexPath) as? RecipeCell {
+            let recipe = recipes[indexPath.item]
+            cell.configureCell(recipe: recipe)
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    // dynamically set the size of the collection view cells
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // set
+        let width = view.bounds.width
+        let cellDimension = (width / 2) - 15
+        return CGSize(width: cellDimension, height: cellDimension)
     }
 
 }
